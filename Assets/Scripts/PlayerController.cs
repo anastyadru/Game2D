e583ce52ee509647829b0b7707479156767146c8
Nastia <adruts@mail.ru>
2023-12-10 23:47:07 +0300

    https://github.com/anastyadru/Game2D.git

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
+    Rigidbody2D rb;
     
     void Start()
     {
-
+        rb = GetComponent<
     }
 
     void Update()
     {
-
+        float movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
     }
 }
