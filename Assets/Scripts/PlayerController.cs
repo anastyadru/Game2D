945f06b78d3ff46f492aec252b720b87014e761a
Nastia <adruts@mail.ru>
2023-12-10 23:34:37 +0300

    https://github.com/anastyadru/Game2D.git

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
